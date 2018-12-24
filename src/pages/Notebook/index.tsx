636233d56c4e9a463f8c969fd80edd4523c51c28
marquisf <marquisfffff@gmail.com>
2018-12-24 09:43:30 +0800

    basic functions

--- src/pages/Notebook/index.tsx
+++ src/pages/Notebook/index.tsx
@@ -1,18 +1,56 @@
 import * as React from 'react';
 
-export default class App extends React.Component<NotebookProps> {
+export default class App extends React.Component<NotebookProps, NotebookState> {
+
+  private inputElement: React.RefObject<HTMLInputElement>
+
+  constructor(props: NotebookProps) {
+      super(props);
+      this.inputElement = React.createRef();
+  }
+
   public state: NotebookState = {
     IOhistoryList: []
   }
 
+  private enter = async (
+    e: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    e.preventDefault();
+    this.setState((prevState: NotebookState): NotebookState => {
+      let inputContent: string = this.inputElement.current ?
+        this.inputElement.current.value :
+        '';
+      this.inputElement.current && (this.inputElement.current.value = '');
+
+      prevState.IOhistoryList.push({
+        type: IOItemTypes.INPUT,
+        content: inputContent
+      });
+
+      return { IOhistoryList: prevState.IOhistoryList };
+    })
+  };
+
   render() {
+    const { IOhistoryList } = this.state;
+
     return (
       <div>
-        <h2>Note book</h2>
-        <form onSubmit={this.}>
+        <h2>Notebook</h2>
+        <ul>
+        {
+          IOhistoryList.map((value: IOItem, index: number): JSX.Element => (
+            <li key={index}>
+              {value.content}
+            </li>
+          ))
+        }
+        </ul>
+        <form onSubmit={this.enter}>
           <label>#</label>
-          <input type='text' />
-          <button>send</button>
+          <input type='text' ref={this.inputElement} />
+          <button>Enter</button>
          </form>
       </div>
     )
@@ -24,7 +62,7 @@ export interface NotebookProps {
 }
 
 export interface NotebookState {
-  IOhistoryList: Array<any>
+  IOhistoryList: Array<IOItem>
 }
 
 export interface IOItem {
